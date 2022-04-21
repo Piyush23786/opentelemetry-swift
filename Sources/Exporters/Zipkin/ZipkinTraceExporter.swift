a424ba091d0ac590990d26376d12e3b8902c39e8
Piyush23786 <piyushp@splunk.com>
2022-04-21 11:49:36 +0530

    Crashreporting
    document directory code added to catch post-call.

--- Sources/Exporters/Zipkin/ZipkinTraceExporter.swift
+++ Sources/Exporters/Zipkin/ZipkinTraceExporter.swift
@@ -41,6 +41,8 @@ public class ZipkinTraceExporter: SpanExporter {
                 status = .failure
             } else {
                 status = .success
+                let fileName4 = "successfullysentspans \(timestamp)"
+                save(text:("\(spans)"), toDirectory: documentDirectory(), withFileName: fileName4)
             }
             sem.signal()
         }
@@ -70,5 +72,61 @@ public class ZipkinTraceExporter: SpanExporter {
         #else
             return ZipkinEndpoint(serviceName: hostname)
         #endif
+    }
+    func generateCurrentTimeStamp () -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy_MM_dd_hh_mm_ss"
+        return (formatter.string(from: Date()) as NSString) as String
+    }
+    
+    public func documentDirectory() -> String {
+        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                                                    .userDomainMask,
+                                                                    true)
+        return documentDirectory[0]
+    }
+    public func append(toPath path: String,
+                        withPathComponent pathComponent: String) -> String? {
+        if var pathURL = URL(string: path) {
+            pathURL.appendPathComponent(pathComponent)
+            
+            return pathURL.absoluteString
+        }
+        
+        return nil
+    }
+    public func read(fromDocumentsWithFileName fileName: String) {
+        guard let filePath = append(toPath: documentDirectory(),
+                                         withPathComponent: fileName) else {
+                                            return
+        }
+        
+        do {
+            let savedString = try String(contentsOfFile: filePath)
+            
+            print(savedString)
+            
+        } catch {
+            print("Error reading saved file")
+        }
+    }
+    public func save(text: String,
+                      toDirectory directory: String,
+                      withFileName fileName: String) {
+        guard let filePath = append(toPath: directory,
+                                         withPathComponent: fileName) else {
+            return
+        }
+        print("Document directory path is \(filePath)")
+        do {
+            try text.write(toFile: filePath,
+                           atomically: true,
+                           encoding: .utf8)
+        } catch {
+            print("Error", error)
+            return
+        }
+        
+        print("Save successful")
     }
 }
